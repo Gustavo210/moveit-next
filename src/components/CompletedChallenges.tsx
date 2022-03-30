a404f34cedd9f8d4ecfd248950270fe34527e499
Gustavo Aurélio <44369818+Gustavo210@users.noreply.github.com>
2022-03-30 19:59:43 -0300

    corrigindo

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -6,7 +6,7 @@ export function CompletedChallenges() {
   const { challengesCompleted } = useContext(ChallengesContext);
   return (
     <div className={styles.completedChallengesContainer}>
-      <span>Disafios completos</span>
+      <span>Desafios completos</span>
       <span>{challengesCompleted}</span>
     </div>
   );
